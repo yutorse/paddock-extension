2e5299ff52a7fff93c49ff1278edfc95dea2663f
yutorse <yutohorse.coc@gmail.com>
2024-04-26 11:17:07 +0900

    let -> const

--- src/scriptS.js
+++ src/scriptS.js
@@ -13,18 +13,18 @@ const raceCourseId = {
   小倉: "a",
 };
 
-let url = "https://regist.prc.jp/api/windowopen.aspx?target=race/";
+const url = "https://regist.prc.jp/api/windowopen.aspx?target=race/";
 
-let raceInfo = document.getElementsByClassName("opt")[2].innerHTML;
-let year = raceInfo.split("年")[0];
-let month = ("0" + raceInfo.split("年")[1].split("月")[0]).slice(-2);
-let day = ("0" + raceInfo.split("年")[1].split("月")[1].split("日")[0]).slice(
+const raceInfo = document.getElementsByClassName("opt")[2].innerHTML;
+const year = raceInfo.split("年")[0];
+const month = ("0" + raceInfo.split("年")[1].split("月")[0]).slice(-2);
+const day = ("0" + raceInfo.split("年")[1].split("月")[1].split("日")[0]).slice(
   -2
 );
-let kaisaiNumber = raceInfo.split("回")[0].slice(-1);
-let kaisaiCourse = raceInfo.split("回")[1].slice(0, 2);
-let kaisaiDay = raceInfo.split("回")[1].split(" ")[0].slice(2, -1);
-let raceNumber = raceInfo.split(" ")[1].slice(0, -3);
+const kaisaiNumber = raceInfo.split("回")[0].slice(-1);
+const kaisaiCourse = raceInfo.split("回")[1].slice(0, 2);
+const kaisaiDay = raceInfo.split("回")[1].split(" ")[0].slice(2, -1);
+const raceNumber = raceInfo.split(" ")[1].slice(0, -3);
 
 url =
   url +
@@ -41,9 +41,9 @@ url =
   Number(raceNumber).toString(16) +
   "_p&quality=4";
 
-let oddsButtonElement = document.getElementsByClassName(
+const oddsButtonElement = document.getElementsByClassName(
   "btn-def btn-sm blue btn-block"
 )[0];
-let paddockButtonElement = `<a href=${url} target="_blank" id='paddock-button' class='btn-def btn-sm blue btn-block mt5'><i class='fa fa-chevron-circle-right'></i>パドック</a>`;
+const paddockButtonElement = `<a href=${url} target="_blank" id='paddock-button' class='btn-def btn-sm blue btn-block mt5'><i class='fa fa-chevron-circle-right'></i>パドック</a>`;
 
 oddsButtonElement.insertAdjacentHTML("afterend", paddockButtonElement);
